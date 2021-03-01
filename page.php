dc27cffea6efed752de2e3d2df4c958206f670a8
Wayne McWilliams <waynewebdev@gmail.com>
2021-03-01 12:20:32 -0600

    Create page.php

---
+++ page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Wayne
+ */
+
+get_header(); 
+
+get_template_part( 'template-parts/secondary-nav' );
+get_template_part( 'template-parts/page', 'title' );
+
+  if ( !empty( get_the_content() ) ) : 
+
+            while ( have_posts() ) : the_post();
+              get_template_part( 'template-parts/content', 'page' );
+		        endwhile; // End of the loop.
+
+  endif; 
+
+echo get_template_part( 'template-parts/modules' );
+
+get_footer();
